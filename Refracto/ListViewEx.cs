cd2db984de27ae89e8441dc8201a003e283952d7
Pavel Zaytsev <pavel@iberisoft.com>
2019-06-06 13:39:03 +0200

    Fix issue #5

---
+++ Refracto/ListViewEx.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Specialized;
+using System.Windows.Controls;
+
+namespace Refracto
+{
+    public class ListViewEx : ListView
+    {
+        protected override void OnItemsChanged(NotifyCollectionChangedEventArgs e)
+        {
+            var gridView = View as GridView;
+            if (gridView != null)
+            {
+                foreach (var column in gridView.Columns)
+                {
+                    column.Width = column.ActualWidth;
+                    column.Width = double.NaN;
+                }
+            }
+
+            base.OnItemsChanged(e);
+        }
+    }
+}
