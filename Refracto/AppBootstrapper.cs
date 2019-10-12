787ad009092b8df90f7ab3848cfe7744fbdeab05
Pavel Zaytsev <pavel@iberisoft.com>
2019-10-12 10:34:09 +0200

    Simplify bindings

--- Refracto/AppBootstrapper.cs
+++ Refracto/AppBootstrapper.cs
@@ -1,8 +1,11 @@
 using Autofac;
+using Caliburn.Micro;
 using Caliburn.Micro.Autofac;
 using Refracto.Services;
 using Refracto.ViewModels;
+using System.Linq;
 using System.Windows;
+using Xceed.Wpf.Toolkit;
 
 namespace Refracto
 {
@@ -18,6 +21,23 @@ namespace Refracto
             base.ConfigureBootstrapper();
 
             EnforceNamespaceConvention = false;
+
+            var valueConvention = ConventionManager.AddElementConvention<IntegerUpDown>(IntegerUpDown.ValueProperty, "Value", "ValueChanged");
+            var baseBindProperties = ViewModelBinder.BindProperties;
+            ViewModelBinder.BindProperties = (elements, viewModelType) =>
+            {
+                foreach (var element in elements.OfType<IntegerUpDown>())
+                {
+                    var propertyName = element.Name;
+                    var property = viewModelType.GetPropertyCaseInsensitive(propertyName);
+                    if (property != null)
+                    {
+                        ConventionManager.SetBindingWithoutBindingOverwrite(viewModelType, propertyName, property, element, valueConvention, valueConvention.GetBindableProperty(element));
+                    }
+                }
+
+                return baseBindProperties(elements, viewModelType);
+            };
         }
 
         protected override void ConfigureContainer(ContainerBuilder builder)
